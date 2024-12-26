f17fee5a46d9bce55306f751903bbdad556fd793
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-12-26 19:15:07 +0900

    chore: update warn message

--- src/processing/lwjgl/PLWJGL.java
+++ src/processing/lwjgl/PLWJGL.java
@@ -458,14 +458,14 @@ public class PLWJGL extends PGL {
   @Override
   protected int maxSamples() {
     // FIXME: sampler is not same as multisample
-    logWarningOnce("FIXME: maxSamples()", "maxSamples() returns maxTextureSamplers() = " + getLimits().maxTextureSamplers() + ", which may not the number of multisamples.");
+    logWarningOnce("maxSamples()", "FIXME: maxSamples() returns maxTextureSamplers() = " + getLimits().maxTextureSamplers() + ", which may not the number of multisamples.");
     return getLimits().maxTextureSamplers();
   }
 
   @Override
   protected int getMaxTexUnits() {
     // FIXME: maxTextures() returns the maximum texture size, may not the number of texture units
-    logWarningOnce("FIXME: getMaxTexUnits()", "maxTextureUnits() returns maxTexures() = " + getLimits().maxTextures() + "");
+    logWarningOnce("getMaxTexUnits()", "FIXME: maxTextureUnits() returns maxTexures() = " + getLimits().maxTextures() + "");
     return getLimits().maxTextures();
     // return getLimits().maxTextureSize();
   }
